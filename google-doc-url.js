3eaef315c69c17aafc605dbb4889f6f761ebe131
neiugis <t-liu1@neiu.edu>
2023-11-09 15:15:57 -0600

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRkM2mOF7ldtN2TmP1YzYsBHPpmpxH-qqQ88m39E_E5xOpB0gezyOIZX8Sj2K4DJMggA98vHdVipJes/pubhtml';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
